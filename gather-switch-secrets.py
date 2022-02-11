c9c31cf41d956000c13e41918bc419271e427586
KalDrongo <garret.fs@gmail.com>
2022-02-11 21:27:21 +0000

    This is the finished version of the script that should work with the instructions in the README

--- gather-switch-secrets.py
+++ gather-switch-secrets.py
@@ -1,9 +1,23 @@
 import getpass
 import pexpect
+import json
+import os
 
+#Get manager's password
 secret_phrase = str(getpass.getpass())
 
-invoker = pexpect.spawn("scp -o KexAlgorithms=+diffie-hellman-group14-sha1 manager@128.111.111.20:/cfg/running-config .")
-invoker.expect("password:")
-invoker.sendline(secret_phrase)
-invoker.expect(pexpect.EOF)
+# Opening JSON switch_file
+switch_file = open('switches.json',)
+# returns JSON object as a dictionary
+book_of_switches = json.load(switch_file)
+
+for switch in book_of_switches:
+  print("Trying to connect to: " + switch["NAME"])
+  invoker = pexpect.spawn("scp -o KexAlgorithms=+diffie-hellman-group14-sha1 manager@" + switch["IP"] + ":/cfg/running-config .")
+  invoker.expect("password:")
+  invoker.sendline(secret_phrase)
+  invoker.expect(pexpect.EOF)
+  os.system("mv running-config configs/" + switch["NAME"] + "-config")
+
+# Closing JSON switch_file
+switch_file.close()
